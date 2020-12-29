b3186dfbe7e405d62a63fe6749a5f2dcddb68c0b
jsu800 <jsu800@gmail.com>
2020-12-29 15:59:57 -0800

    adding javascript global var change in jsx

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 
+var world_text = "world!!!!!!!!!!!!!!";
+
 function App() {
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello {world_text}
         </p>
         <a
           className="App-link"
