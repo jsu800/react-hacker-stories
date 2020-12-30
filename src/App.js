f9d3962a26a3dd0bb326476875181378ecd2a51a
jsu800 <jsu800@gmail.com>
2020-12-29 18:38:53 -0800

    instantiating List components

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 
-const list = [
-  {
-    title: 'React Homepage',
-    url: 'https://reactjs.org/',
-  },
-  {
-    title: 'Getting Started',
-    url: 'https://reactjs.org/docs/getting-started.html',
-  },
-];
+class Sentence {
+  constructor(word1, word2) {
+    this.word1 = word1;
+    this.word2 = word2;
+  }
 
+  getSentence() {
+    return this.word1 + ' ' + this.word2;
+  }
+}
 
 function App() {
 
@@ -20,6 +19,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
+          {/* creating/instantiating an instance of List component */}
+          <List />
+          {/* creating/instantiating ANOTHER instance of List component */}
           <List />
         </p>
       </header>
@@ -28,8 +30,16 @@ function App() {
 }
 
 function List() {
-  return list.map(item =>
-    <div><a href={item.url}>{item.title}</a></div>)
+  const hello_world = new Sentence('Hello', 'World!');
+  const react_rocks = new Sentence('React', 'rocks!');
+
+  return (
+    <div>
+      {hello_world.getSentence()}
+      <hr />
+      {react_rocks.getSentence()}
+    </div>
+  );
 }
 
 
