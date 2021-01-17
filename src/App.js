aceb77e4d9da1a035f2da840dcc52c80879cc5d2
jsu800 <jsu800@gmail.com>
2021-01-17 10:39:08 -0800

    template code for search callback

--- src/App.js
+++ src/App.js
@@ -1,41 +1,18 @@
 import React from 'react';
 
+// This is your callback from Search input. You will
+// take the event and print its value to the console 
+// log
+const handleSearch = YOUR_CODE_HERE
+
 const App = () => {
-
-  const toc = [
-    {
-      title: 'React Homepage',
-      url: 'https://reactjs.org/',
-    },
-    {
-      title: 'Getting Started',
-      url: 'https://reactjs.org/docs/getting-started.html',
-    },
-    {
-      title: 'Hello World',
-      url: 'https://reactjs.org/docs/hello-world.html',
-    },
-    {
-      title: 'Introducing JSX',
-      url: 'https://reactjs.org/docs/introducing-jsx.html',
-    },
-    {
-      title: 'Rendering Elements',
-      url: 'https://reactjs.org/docs/rendering-elements.html',
-    }
-  ];
-
   return (
     <div>
       <h1>React: A JavaScript library</h1>
 
-      // Creating a search component
-      YOUR_CODE_HERE
+      // Creating a search component with a props
+      <Search onSearch={handleSearch} />
 
-      <hr />
-
-      // Creating a List component
-      <List results = {toc}/>
     </div>
   );
 };
@@ -43,16 +20,8 @@ const App = () => {
 const Search = props => (
   <div>
     <label htmlFor="search">Search: </label>
-    <input type="text" />
+    <input id="search" type="text" onChange={props.onSearch}/>
   </div>
 );
-
-const List = props =>
-  props.results.map(item => (
-    <div>
-      YOUR_CODE_HERE
-    </div>
-  ));
-
 
 export default App;
