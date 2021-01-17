12567aa7bc619cf48cf1b70dc9d7c645825ed331
jsu800 <jsu800@gmail.com>
2021-01-17 00:16:07 -0800

    template code

--- src/App.js
+++ src/App.js
@@ -1,83 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
 import React from 'react';
 
-class Sentence {
-  constructor(word1, word2) {
-    this.word1 = word1;
-    this.word2 = word2;
-  }
-
-  getSentence() {
-    return this.word1 + ' ' + this.word2;
-  }
-}
-
-// function declaration 
-// function name() {...}
-
-// arrow function: 
-// const name() => { ... } 
-
-
 const App = () => {
-
-  const handleInput = evt => {
-    setCheckTerm(evt.target.value);
-    console.log(evt.target.value);
-  };
-
-  const hello_world = new Sentence('Hello', 'World!');
-  const react_rocks = new Sentence('React', 'rocks!');
-
-  const [checkTerm, setCheckTerm] = React.useState(
-    localStorage.getItem("input") || "nothing"
-  );
-
-  React.useEffect(
-    ()=> {
-      localStorage.setItem("input", checkTerm);
-    }, 
-    [checkTerm]
-  );
-
-  
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          {/* creating/instantiating an instance of List component */}
-          <List first={hello_world} second={react_rocks} />
-        </p>
-        <p>
-          <Input onInput = {handleInput} userInput = {checkTerm} />
-        </p>
-      </header>
-    </div>
-  );
-
-};
-
-const Input = props => {  
   return (
     <div>
-      <label htmlFor="Check">Check: </label>
-      <input id="check" type="text" value={props.userInput} onChange={props.onInput} />
-      <p>Checking for <b>{props.userInput}</b></p>
+      <Title YOUR_CODE_HERE />
+      <SubTitle YOUR_CODE_HERE/>
     </div>
   );
+};
+
+const Title = props => (
+  <h1>{YOUR_CODE_HERE}</h1>
+);
+
+function SubTitle() {
+  return <p>A JavaScript library for building user interfaces.</p>;
 }
-
-const List = props => {
-  return (
-    <div>
-      {props.first.getSentence()}
-      <hr />
-      {props.second.getSentence()}
-    </div>
-  );
-};
-
 
 export default App;
