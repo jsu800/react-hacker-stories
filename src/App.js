c8611fc99fd86ee800536ead2b76510c5232047a
jsu800 <jsu800@gmail.com>
2021-01-27 22:23:59 -0800

    promise

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
-const list = [
+const initialBookmarks = [
   {
     title: 'React Homepage',
     url: 'https://reactjs.org/',
@@ -12,23 +13,38 @@ const list = [
   },
 ];
 
+const getAsyncBookmarks = () =>
+  new Promise(resolve =>
+    setTimeout(
+      () => resolve({ data: { bookmarks: initialBookmarks } }),
+      3000
+    )
+  );
 
 function App() {
+
+  const [bookmarks, setBookmarks] = React.useState([]);
+
+  React.useEffect(() => {
+    getAsyncBookmarks().then(result => {
+      setBookmarks(result.data.bookmarks);
+    });
+  }, []);
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          <List />
+          <List links = {bookmarks} />
         </p>
       </header>
     </div>
   );
 }
 
-function List() {
-  return list.map(item =>
+function List({links}) {
+  return links.map(item =>
     <div><a href={item.url}>{item.title}</a></div>)
 }
 
