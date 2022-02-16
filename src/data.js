c9fe4b7873f4d5c62615d26cfea7fa273c85362a
jsu800 <jsu800@gmail.com>
2022-02-16 12:21:42 -0800

    adding base files for assignment

---
+++ src/data.js
@@ -0,0 +1,41 @@
+export default [
+    {
+        id: 1,
+        title: "Nutrition info about Apple",
+        description: "An apple is an edible fruit produced by an apple tree. Apple trees are cultivated worldwide and are the most widely grown species in the genus Malus.",
+        price: 1.00,
+        coverImg: "apple.jpg",
+        stats: {
+            kingdom: "Plantae",
+            family: "Rosaceae",
+            genus: "Malus"
+        },
+        location: "Japan"
+    },
+    {
+        id: 2,
+        title: "Avocado Farms",
+        description: "Interested in becoming a wedding photographer? For beginner and experienced photographers alike, join us in learning techniques required to leave the happy couple with memories that'll last a lifetime.",
+        price: 1.50,
+        coverImg: "avocado.jpg",
+        stats: {
+            kingdom: "Plantae",
+            family: "Lauraceae",
+            genus: "Persea"
+        },
+        location: "Mexico"
+    },
+    {
+        id: 3,
+        title: "Orange Grove the King",
+        description: "Oranges are round orange-coloured fruit that grow on a tree which can reach 10 metres (33 ft) high.",
+        price: 1.25,
+        coverImg: "orange.jpg",
+        stats: {
+            kingdom: "Plantae",
+            family: "Rutaceae",
+            genus: "Citrus"
+        },
+        location: "United States"
+    }
+]
