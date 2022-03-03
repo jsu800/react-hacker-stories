66d98d81d0dc07c520ef268843bf5ae3f4795036
jsu800 <jsu800@gmail.com>
2022-03-03 15:18:27 -0800

    initial project files for the assignment

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useReducer } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const products = [
+  {
+    emoji: '🍦',
+    name: 'ice cream',
+    price: 10
+  },
+  {
+    emoji: '🍩',
+    name: 'donuts',
+    price: 12.5,
+  },
+  {
+    emoji: '🍉',
+    name: 'watermelon',
+    price: 8
+  }
+];
+
+// --------------------------------------------
+// !!! DO NOT MODIFY ABOVE THIS LINE !!!
+// --------------------------------------------
+
+// YOUR MAY ADD ANY ADDITIONAL FUNCTIONS HERE IF YOU NEED, NAMELY THE computeTotal(), etc.
+
+function reducer(state, action) {
+  switch(action.type) {
+    // YOUR CODE HERE
+  }
 }
 
+const App = () => {
+
+  const [state, dispatch] = useReducer(reducer, []);
+
+  // YOUR CODE HERE
+
+// --------------------------------------------
+// !!! DO NOT MODIFY BELOW THIS LINE !!!
+// --------------------------------------------
+  return(
+    <>
+      <div>
+        Total Items in Shopping Cart: {state.length}
+      </div>
+      <div>Total Cost: ${computeTotal(state)}</div>
+
+      <div>
+        {products.map(product => (
+          <div key={product.name}>
+            <div className="product">
+              <span>{product.emoji}</span>
+            </div>
+            <div>Price ${product.price}</div>
+            <button onClick={() => add(product)}>Add</button>
+            <button onClick={() => remove(product)}>Remove</button>
+          </div>
+        ))}
+      </div>
+    </>
+  )
+}
 export default App;
